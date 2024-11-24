82cfb386ec4ca687fbdc0103665787c6b27133f8
Odysseus326 <odysseus326@live.com>
2024-11-23 18:43:03 -0800

    Updates

--- src/components/InnerContainer.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './innerContainer.module.css'
-
-export default function InnerContainer({children}) {
-    return (
-        <div className={styles.innerContainer}>
-            {children}
-        </div>
-    )
-}
